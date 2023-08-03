18baf7278050296b00c19a8c6dc62141e727dd66
Mohammed Yehia <mohammedyehia99@gmail.com>
2023-08-03 23:33:53 +0300

    inital configuration

---
+++ src/test/java/com/moh/yehia/testing/SpringBootTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moh.yehia.testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
