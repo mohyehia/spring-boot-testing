01622cbba9893e5d4b80469e6141c855dd419e39
mohyehia <mohammedyehia@gmail.com>
2024-06-19 12:04:41 +0300

    update properties

--- src/test/java/com/moh/yehia/testing/repository/BaseMongoContainer.java
+++ src/test/java/com/moh/yehia/testing/repository/BaseMongoContainer.java
@@ -5,11 +5,14 @@ import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.utility.DockerImageName;
 
+import java.util.List;
+
 public abstract class BaseMongoContainer {
     static final MongoDBContainer MONGO_DB_CONTAINER;
 
     static {
-        MONGO_DB_CONTAINER = new MongoDBContainer(DockerImageName.parse("mongo:latest"));
+        MONGO_DB_CONTAINER = new MongoDBContainer(DockerImageName.parse("mongo:4.4.29-focal"));
+        MONGO_DB_CONTAINER.setPortBindings(List.of("27017:27017")); // used for only enforcing testcontainers to create the expose port 27017 for the mongo container
         MONGO_DB_CONTAINER.start();
     }
 
